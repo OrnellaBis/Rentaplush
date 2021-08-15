d77e670fec2fa254ae207e907956bb82eaf2a70f
benoit jeanjoseph <benoit.jeanjoseph@gmail.com>
2021-08-15 06:38:03 +0200

    create-action

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,15 +1,32 @@
 class BookingsController < ApplicationController
     def index
-        
+        @bookings = Booking.all
     end
     def show
         
     end
     def create
+        @user = User.find(params[:user_id])
+        @plush = Plush.find(params[:plush_id])
+        @booking = Booking.new(booking_params)
+        @booking.user = @user
+        @booking.plush = @plush
+        if @booking.save
+            redirect_to bookings_path
+        else
+            render "/plush/#{@plush}"
+        end
         
     end
     def destroy
-        
+        @booking = Booking.find(params[:id])
+        @booking.destroy
+        redirect_to bookings_path
     end
     
+    private
+
+    def booking_params
+        params.require(:booking).permit(:start_date, :end_date, :user_id, :plush_id)
+    end
 end
