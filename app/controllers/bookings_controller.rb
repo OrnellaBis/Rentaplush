6797f47b9b031adfc9aa94442fcdb1cb8574aa7f
benoit jeanjoseph <benoit.jeanjoseph@gmail.com>
2021-08-16 04:03:49 +0200

    corresction booking create

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -15,7 +15,7 @@ class BookingsController < ApplicationController
         if @booking.save
           redirect_to bookings_path
         else
-          render "/plush/#{@plush}"
+          redirect_to plush_path(@plush)
         end
     end
 
