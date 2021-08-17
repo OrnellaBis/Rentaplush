729c00888539a4da3f64084e76bf01197195e78a
Andrea Prouvost <andrea.prouvost@gmail.com>
2021-08-17 17:50:49 +0200

    add cards and index ok

--- app/controllers/plushes_controller.rb
+++ app/controllers/plushes_controller.rb
@@ -1,9 +1,9 @@
 class PlushesController < ApplicationController
-    def index
-        
-    end
-    def show
-        
-    end
-    
+  def index
+    @plushes = Plush.all
+  end
+  def show
+
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-require 'faker'
 
-puts 'Creating 100 fake plushes...'
-
-100.times do
-  plush = Plush.new(
-    name: Faker::Name.name,
-    category: Faker::Creature::Animal.name,
-    description: Faker::Quote,
-    localisation: Faker::Address.city,
-    size: rand(1..10),
-    price_per_day: rand(15..100),
-    user_id: 1
-  )
+  plush = Plush.new(name: "Bébé Yoda", category: "Films", description: "Cette peluche est une adorable créature pour vivre des aventures intergalactiques et pleines de calins.", localisation: "Paris 15ème", size: rand(1..10), price_per_day: rand(15..100), user_id: 1)
+  plush_1 = Plush.new(name: "Edward", category: "Animaux", description: "Un bébé renard tout mignon et tout soyeux", localisation: "Bordeaux", size: rand(1..10), price_per_day: rand(15..100), user_id: 1)
+  plush_2 = Plush.new(name: "Belby Toucan", category: "Animaux", description: "Un toucan qui ne fait pas de boucan.", localisation: "Limoges", size: rand(1..10), price_per_day: rand(15..100), user_id: 1)
+  plush_3 = Plush.new(name: "Kim", category: "Kawaï", description: "Kim K.", localisation: "Lyon", size: rand(1..10), price_per_day: rand(15..100), user_id: 1)
+  plush_4 = Plush.new(name: "Smili", category: "Emoji", description: "Smili vous rendra instantanément le sourire.", localisation: "Hossegor", size: rand(1..10), price_per_day: rand(15..100), user_id: 1)
   plush.save!
-end
-puts 'Finished!'
+  plush_1.save!
+  plush_2.save!
+  plush_3.save!
+  plush_4.save!
