9c2ed6f57fb5330a36997d686d7c2f737246b529
Fatima-zahra Aamrane <aamrane.fz@gmail.com>
2021-08-17 15:21:43 +0200

    done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,16 +2,19 @@ class BookingsController < ApplicationController
     def index
         @bookings = Booking.all
     end
+
     def show
-        
     end
+
+
+
     def create
-        
     end
+
     def destroy
         @booking = Booking.find(params[:id])
         @booking.destroy
         redirect_to bookings_path
     end
-    
+
 end
