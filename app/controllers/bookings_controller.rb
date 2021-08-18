ff658ad82410176589bdf4b7f754bd684ae8842c
Julien Guichard <guichardjulien@hotmail.fr>
2021-08-18 12:20:35 +0200

    commit

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,15 +1,33 @@
 class BookingsController < ApplicationController
-    def index
-        
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+  end
+
+  def create
+    @user = current_user
+    @plush = Plush.find(params[:plush_id])
+    @booking = Booking.new(booking_params)
+    @booking.user = @user
+    @booking.plush = @plush
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render "/plush/#{@plush}"
     end
-    def show
-        
-    end
-    def create
-        
-    end
-    def destroy
-        
-    end
-    
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to bookings_path
+  end
+    private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :plush_id)
+  end
+
 end
--- app/controllers/plushes_controller.rb
+++ app/controllers/plushes_controller.rb
@@ -1,9 +1,10 @@
 class PlushesController < ApplicationController
-    def index
-        
-    end
-    def show
-        
-    end
-    
+  def index
+    @plushes = Plush.all
+  end
+
+  def show
+    @plush = Plush.find(params[:id])
+    @booking = Booking.new
+   end
 end
