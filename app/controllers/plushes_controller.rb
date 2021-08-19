8705651d625901241514e4926b4cc1df3cbbeb24
Julien Guichard <guichardjulien@hotmail.fr>
2021-08-19 16:32:59 +0200

    inject mapbox

--- app/controllers/plushes_controller.rb
+++ app/controllers/plushes_controller.rb
@@ -2,10 +2,12 @@ class PlushesController < ApplicationController
   def index
     @plushes = Plush.all
 
-    @markers = @plushes.geocoded.map do |flat|
+    @markers = @plushes.geocoded.map do |plush|
       {
-        lat: flat.latitude,
-        lng: flat.longitude
+        lat: plush.latitude,
+        lng: plush.longitude,
+        info_window: render_to_string(partial: "info_window", locals: { plush: plush })
+        # image_url: helpers.asset_url('REPLACE_THIS_WITH_YOUR_IMAGE_IN_ASSETS')
       }
     end
   end
