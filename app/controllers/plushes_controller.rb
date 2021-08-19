12109a0fc3db21d58723028b8986169ed45ae6a8
Ornella Bissessur <ornella.bissessur@gmail.com>
2021-08-19 18:19:14 +0200

    searchbar

--- app/controllers/plushes_controller.rb
+++ app/controllers/plushes_controller.rb
@@ -1,10 +1,15 @@
 class PlushesController < ApplicationController
   def index
+    if params[:query].present?
+      sql_query = "category ILIKE :query OR description ILIKE :query OR localisation ILIKE :query"
+      @plushes = Plush.where(sql_query, query: "%#{params[:query]}%")
+      # @plushes = Plush.where("category ILIKE ?", "%#{params[:query]}%")
+    else
     @plushes = Plush.all
+    end
   end
   def show
     @plush = Plush.find(params[:id])
     @booking = Booking.new
   end
-
 end
