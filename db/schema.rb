a7e000e561e193d38a994fbdc3428923cb90fc29
benoit jeanjoseph <benoit.jeanjoseph@gmail.com>
2021-08-19 15:08:29 +0200

    bundl et yarn

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2021_08_17_092804) do
-=======
 ActiveRecord::Schema.define(version: 2021_08_19_123318) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
