24b73c4e91ce6d8240916933801340f1c39b45cc
Ornella Bissessur <ornella.bissessur@gmail.com>
2021-08-20 12:00:21 +0200

    error correct

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,7 @@
 class BookingsController < ApplicationController
     def index
       @bookings = Booking.all
+      @mounth = ["none", "Janvier", "Février", "Mars", "Arvil", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Décembre"]
     end
 
     def show
